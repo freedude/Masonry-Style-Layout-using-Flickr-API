690b432580703703480956517ddb5e22f10aa277
freedude <seanfreemantya@gmail.com>
2019-01-13 21:17:25 +0000

    Final Commit

--- src/app/HomePage/home.component.ts
+++ src/app/HomePage/home.component.ts
@@ -13,18 +13,15 @@ export class HomeComponent implements OnInit {
 
     public searchControl = new FormControl();
     public photos: Object;
-    public edited = false;
+
+    public page = 1;
+    public searchTerm;
 
     constructor(private flickrService: FlickrService) { }
 
-    onScroll() {
-        this.edited = true;
-    }
-
     ngOnInit() {
-
         // Images on first load
-        this.flickrService.getResult('ireland').subscribe(value => {
+        this.flickrService.getResult('ireland', this.page).subscribe(value => {
             this.photos = value;
         });
 
@@ -32,15 +29,28 @@ export class HomeComponent implements OnInit {
         this.searchControl.valueChanges
             .pipe(debounceTime(400))
             .pipe(distinctUntilChanged())
-            .pipe(switchMap((search: string) => this.flickrService.getResult(search)))
+            .pipe(switchMap((search: string) => this.flickrService.getResult(search, this.page)))
             .subscribe(value => {
                 this.photos = value;
                 
-            }
+            });
+    }
 
-            );
+    // Increase number of pages for infinite scrolling
+    onScroll() {
+        
+        this.page++;
+        this.searchTerm = this.searchControl.value;
+       
+        if (this.searchTerm === null) {
+            this.searchTerm = 'ireland';  
+        }
 
+        this.flickrService.getResult(this.searchTerm, this.page).subscribe(value => {
+            this.photos = value;
+        });
 
     }
+
 
 }
--- src/app/flickr.service.ts
+++ src/app/flickr.service.ts
@@ -1,46 +1,51 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class FlickrService {
-    result: Observable<string[]>;
-    key = 'a62faaa60f5e3f6408d527d3c3a2d98a';
-    
-    constructor(private http: HttpClient) {}
-   
 
-    getResult(query: string) {
+    public key = 'a62faaa60f5e3f6408d527d3c3a2d98a';
+    public resultsPerPage;
+
+    constructor(private http: HttpClient) { }
+
+
+    getResult(query: string, page: any) {
+        if (page === 1) {
+            this.resultsPerPage = 20;
+        } else {
+            this.resultsPerPage = this.resultsPerPage + 20;
+        }
+
         const searchApi = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${this
-            .key}&tags=${query}&extras=owner_name,date_taken,tags&per_page=20&safe_search=1&format=json&nojsoncallback=1`;
+            .key}&tags=${query}&extras=owner_name,date_taken,tags&per_page=${this.resultsPerPage}
+            &safe_search=3&format=json&nojsoncallback=1`;
 
-        console.log('searchApi' + searchApi);
-            return this.http
-                .get(searchApi)
-                .pipe(map((values: any) => {
-                    console.log('PHOTO SEARCH RESULTS' + JSON.stringify(values));
-                    if (values.stat === 'ok') {
-                        return values.photos.photo.map((photo: any) => {
-                            
-                            return {
-                                url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
-                                title: photo.title,
-                                owner: photo.ownername,
-                                id: photo.id, 
-                                dateTaken: photo.datetaken,
-                                tags: photo.tags,
-                            };
-    
-                            
-                        });
-                        
-                    } else {
-                        window.alert('Sorry, there was an error retrieving your photos, please try another search term');
-                        
-                    }
-                }));
-        } 
+        // console.log('searchApi: ' + searchApi);
+        return this.http
+            .get(searchApi)
+            .pipe(map((values: any) => {
+                // console.log('Photo Search Results: ' + JSON.stringify(values));
+                if (values.stat === 'ok') {
+                    return values.photos.photo.map((photo: any) => {
+
+                        return {
+                            url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`,
+                            title: photo.title,
+                            owner: photo.ownername,
+                            id: photo.id,
+                            dateTaken: photo.datetaken,
+                            tags: photo.tags,
+                        };
+
+                    });
+
+                } else {
+                    window.alert('Sorry, there was an error retrieving your photos, please try another search term');
+                }
+            }));
+    }
 
 
 }
